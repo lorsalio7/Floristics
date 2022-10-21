655a38c22e39a90b74da670dd0b63cdc8f194531
Lorsalio <lorsalio7@gmail.com>
2022-10-21 20:09:43 +0500

    Сверстана секция 'Каталог' и добавлена библиотека 'Masonry.js'

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-let a = 9;
+var elem = document.querySelector('.catalog__list');
+
+if(elem) {
+  var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.catalog__item',
+    columnWidth: 350,
+    gutter: 40,
+    horizontalOrder: true,
+    fitWidth: true
+  });
+}
+
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,14 @@
 "use strict";
 
-var a = 9;
+var elem = document.querySelector('.catalog__list');
+
+if (elem) {
+  var msnry = new Masonry(elem, {
+    // options
+    itemSelector: '.catalog__item',
+    columnWidth: 350,
+    gutter: 40,
+    horizontalOrder: true,
+    fitWidth: true
+  });
+}
