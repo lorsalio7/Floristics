2383906330c40e40444317640431311d0a751c2c
Lorsalio <lorsalio7@gmail.com>
2023-01-30 14:25:46 +0500

    Сверстан блок композиции

--- app/js/script.js
+++ app/js/script.js
@@ -11,3 +11,27 @@ if(elem) {
   });
 }
 
+// -----------------------------------------------------------
+
+const compositionThumbs = document.querySelectorAll(".compositions__thumb");
+const compositionBigImageWebp = document.querySelector(".compositions__picture source");
+const compositionBigImageJpg = document.querySelector(".compositions__picture img");
+
+
+compositionThumbs.forEach(item => {
+  item.addEventListener("click", (e) => {
+    let target = e.target;
+    removeActiveClass(compositionThumbs, "compositions__thumb--active");
+    target.classList.add("compositions__thumb--active");
+    let imageSource = target.dataset.fullImage;
+
+    compositionBigImageWebp.srcset = imageSource + ".webp";
+    compositionBigImageJpg.src = imageSource + ".jpg";
+  })
+})
+
+function removeActiveClass(items, activeClass) {
+  for(let i = 0; i < items.length; i++) {
+    items[i].classList.remove(activeClass);
+  }
+}
--- dist/js/script.js
+++ dist/js/script.js
@@ -11,4 +11,25 @@ if (elem) {
     horizontalOrder: true,
     fitWidth: true
   });
+} // -----------------------------------------------------------
+
+
+var compositionThumbs = document.querySelectorAll(".compositions__thumb");
+var compositionBigImageWebp = document.querySelector(".compositions__picture source");
+var compositionBigImageJpg = document.querySelector(".compositions__picture img");
+compositionThumbs.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    var target = e.target;
+    removeActiveClass(compositionThumbs, "compositions__thumb--active");
+    target.classList.add("compositions__thumb--active");
+    var imageSource = target.dataset.fullImage;
+    compositionBigImageWebp.srcset = imageSource + ".webp";
+    compositionBigImageJpg.src = imageSource + ".jpg";
+  });
+});
+
+function removeActiveClass(items, activeClass) {
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove(activeClass);
+  }
 }
